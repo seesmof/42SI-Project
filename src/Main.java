9c5012b17c8bee882182d3e4882bd88135df7988
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-27 20:56:53 +0200

    chore: working working, Jesus thank you

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 import javax.swing.*;
+import org.antlr.v4.runtime.*;
 
 void main() {
-    CharStream stream = new ANTLRInputStream("Jesus is LORD");
-    HelloLexer lexer = new HelloLexer(stream);
-    TokenStream tokenStream = new CommonTokenStream(lexer);
-    HelloParser parser = new HelloParser(tokenStream);
-    ParseTree tree = parser.r();
-
-    System.out.println(tree.toStringTree(parser));
-
     JFrame frame = new JFrame();
     JTextArea textArea = new JTextArea();
     textArea.setBounds(10, 10, 465, 440);
@@ -18,4 +11,7 @@ void main() {
     frame.setLayout(null);
     frame.setResizable(false);
     frame.setVisible(true);
+
+    String input=textArea.getText();
+    CharStream stream = CharStreams.fromString(input);
 }
