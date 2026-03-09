dde65a196ca7aeb528efcf519b1800b38b88f5aa
Oleg Onyshchenko <seesmwork@gmail.com>
2026-03-09 16:41:54 +0200

    feat: duplicate the text from input to output field

--- src/Lexer.g4
+++
@@ -1,7 +0,0 @@
-grammar Lexer;
-
-start: block;
-
-block: '{' data '}' ;
-
-data: [0-9] ;
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,44 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import gen.RustLexer;
 import gen.RustParser;
 
-
-
 public class Main {
-
     public static String appName = "42SI.1 Text Editor";
 
     public static void main(String[] args) {
         JFrame frame = new JFrame(appName);
 
         JTextArea inputField = new JTextArea();
-        JLabel outputLabel = new JLabel("Text Editor");
+        JTextArea outputField = new JTextArea();
 
         frame.add(inputField, BorderLayout.NORTH);
-        frame.add(outputLabel, BorderLayout.CENTER);
+        frame.add(outputField, BorderLayout.SOUTH);
+        outputField.setEditable(false);
 
         frame.setSize(400,400);
         frame.setResizable(false);
         frame.setVisible(true);
+
+        inputField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                outputField.setText(inputField.getText());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                outputField.setText(inputField.getText());
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                outputField.setText(inputField.getText());
+            }
+        });
     }
 }
